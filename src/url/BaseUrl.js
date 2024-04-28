4f8af1c25be53f131e0dc79f95393f63d0c32709
kasinathan28 <way2kasinathb35@gmail.com>
2024-04-28 16:13:08 +0530

    Image Url updated

--- src/url/BaseUrl.js
+++ src/url/BaseUrl.js
@@ -1,3 +1,4 @@
 // exports.BASEURL = "http://localhost:5000"
-exports.IMAGEURL = "http://localhost:5000/uploads"
+// exports.IMAGEURL = "http://localhost:5000/uploads"
 exports.BASEURL ="https://res-server-7eve.onrender.com"
+exports.IMAGEURL = "https://res-server-7eve.onrender.com"
